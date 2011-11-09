66df7001db00a85d04acfae91f277c7e31bc8c45
Graham Rich <richgr@gmail.com>
2011-11-09 14:18:36 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shopt::Application.config.secret_token = 'abba8514827def71a55e73c6ed994945d38abbb7780a33b11c771dde7cf2be75e7cd81fe67d516e0c79e17fb6044435f68a716809d3f6899fc89c199ebffde41'
